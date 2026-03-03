748b338548c39d4b3a373ee420574d1b45ebdcf0
davisp98 <162360885+davisp98@users.noreply.github.com>
2026-03-03 12:49:11 +0530

    Create UC1.java

---
+++ UC1.java
@@ -0,0 +1,10 @@
+public class UC1 {
+    public static void main(){
+		System.out.println("OOPS IN STAR PATTERN:");
+		System.out.println(" *****    *****  *****  ****");
+		System.out.println("*     *  *     * *   *  *");
+		System.out.println("*     *  *     * *****  ****");
+		System.out.println("*     *  *     * *         *");
+		System.out.println(" *****    *****  *      ****");
+	}
+}
